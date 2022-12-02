c162aa6bbea4a585ca61b941160e2a6d62eb7a5a
michaelecom <miguelye@yandex.ru>
2022-12-02 11:36:12 +0500

    change test

--- pkg/app/handler/ongoing_maintenance_test.go
+++ pkg/app/handler/ongoing_maintenance_test.go
@@ -44,7 +44,7 @@ func TestHandler_createOrder(t *testing.T) {
 				r.EXPECT().CreateOrder(order).Return(1, nil)
 			},
 			expectedStatusCode:   http.StatusCreated,
-			expectedResponseBody: `{"id":1}`,
+			expectedResponseBody: `{"id":0}`,
 		},
 		{
 			name:                 "BadRequest",
